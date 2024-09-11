d896e462e436edc3a26fe60c24cda14b0294e4d9
Will-Helliwell <wj.helliwell@gmail.com>
2024-09-11 11:07:05 +0800

    If output type selected as database, grab sources from database.

--- config.php
+++ config.php
@@ -8,31 +8,32 @@ return array(
     // sleep between requests in seconds
     'sleep_between_requests' => 0,
 
-    // output type json or print
-    'output_type' => 'json', // json or print
+    // select output type
+    // 'output_type' => 'json',
+    'output_type' => 'database',
 
     // sources to parse seperated by comma
     'sources' => [
 
          'https://www.nhl.com/mapleleafs/news/',
-     //     'https://www.nhl.com/news',
-     //     'https://www.thestar.com/sports/leafs.html',
-     //     'https://theleafsnation.com/category/news',
-     //     'https://theathletic.com/nhl/',
-     //     'https://www.pensionplanpuppets.com/',
-     //     'https://mapleleafshotstove.com/leafs-news/',
-     //     'https://betweentheposts.ca/',
-     //     'https://centreofleafsnation.com/',
-     //     'https://www.nhlpa.com/news',
-     //     'https://torontosun.com/category/sports/hockey/nhl/toronto-maple-leafs/',
-     //     'https://www.theglobeandmail.com/sports/hockey/',
-     //     'https://nationalpost.com/category/sports/hockey/nhl/',
-     //     'https://www.sportsnet.ca/hockey/nhl/',
-     //     'https://www.tsn.ca/nhl/team-page/toronto-maple-leafs/166',
-     //     'https://www.cbc.ca/sports/hockey/nhl',
-     //     'https://globalnews.ca/tag/nhl/',
-     //     'https://www.ctvnews.ca/sports',
-     //     'https://editorinleaf.com/toronto-maple-leafs-news/',
+         'https://www.nhl.com/news',
+         'https://www.thestar.com/sports/leafs.html',
+         'https://theleafsnation.com/category/news',
+         'https://theathletic.com/nhl/',
+         'https://www.pensionplanpuppets.com/',
+         'https://mapleleafshotstove.com/leafs-news/',
+         'https://betweentheposts.ca/',
+         'https://centreofleafsnation.com/',
+         'https://www.nhlpa.com/news',
+         'https://torontosun.com/category/sports/hockey/nhl/toronto-maple-leafs/',
+         'https://www.theglobeandmail.com/sports/hockey/',
+         'https://nationalpost.com/category/sports/hockey/nhl/',
+         'https://www.sportsnet.ca/hockey/nhl/',
+         'https://www.tsn.ca/nhl/team-page/toronto-maple-leafs/166',
+         'https://www.cbc.ca/sports/hockey/nhl',
+         'https://globalnews.ca/tag/nhl/',
+         'https://www.ctvnews.ca/sports',
+         'https://editorinleaf.com/toronto-maple-leafs-news/',
         
     ],
 
--- scraper.php
+++ scraper.php
@@ -18,34 +18,48 @@ $user_agent = get_config('user_agent');
 $database_connection_details = get_config('database_connection_details');
 $output_type = get_config('output_type');
 
-// // test query
-$database = new \App\DatabaseConnection($database_connection_details);
-$rows = $database->fetchAll("SELECT * FROM test_table");
-foreach ($rows as $row) {
-    print_r($row);
-}
-$database->close();
-if(empty($sources)) {
-    die('No sources found in config.php');
-}
-
+// define variables
 $output_array = [];
+
+// get all sources from the database
+if ($output_type === 'database') {
+    $db = new \App\DatabaseConnection($database_connection_details);
+    $sources = $db->fetchAll("SELECT id, url FROM sources;");
+    $db->close();
+    if (empty($sources)) {
+        die('No sources found in database.');
+    }
+}
+$sources_count = count($sources);
+// echo 'sources length before = ' . $sources_count . PHP_EOL;
+// echo 'sources = ' . PHP_EOL;
+// pp($sources);
+
+//temporarily filter array for only maple leafs
+$sources = array_filter($sources, function ($value, $index) {
+    return $index === 3;
+}, ARRAY_FILTER_USE_BOTH);
+// $sources_count = count($sources); 
+// echo 'sources length after = ' . $sources_count . PHP_EOL;
+
 
 // lets loop through the sources
 foreach ($sources as $source) {
     echo 'source = ' . PHP_EOL;
     pp($source);
 
-    $parsed_array = (new \App\Parser($source, $user_agent))->parse()->getResponse();
+    $source_url = ($output_type === 'json') ? $source : $source['url'];
+
+    $parsed_array = (new \App\Parser($source_url, $user_agent))->parse()->getResponse();
     echo 'parsed_array = ' . PHP_EOL;
     pp($parsed_array);
 
-    $source_domain = str_replace(['https://', 'http://', 'www.'], '', $source);
+    $source_domain = str_replace(['https://', 'http://', 'www.'], '', $source_url);
     echo 'source_domain = ' . PHP_EOL;
     pp($source_domain);
-	
+
     // merge the parsed array with the output array
-    if(!empty($parsed_array)) {
+    if (!empty($parsed_array)) {
         $output_array[$source_domain] = $parsed_array;
     }
 
@@ -56,23 +70,23 @@ foreach ($sources as $source) {
 }
 
 // check if output array is empty
-if(empty($output_array)) {
+if (empty($output_array)) {
     die('No results found');
 }
 
+echo 'output_array = ' . PHP_EOL;
+pp($output_array);
+
 switch ($output_type) {
     case 'json':
-         // add header
         header('Content-Type: application/json');
         echo json_encode($output_array);
-        
         break;
-    
+
     case 'database':
         echo 'Output type specified as database' . PHP_EOL;
-        exit;
         break;
-    
+
     default:
         echo 'Output type has not been specified.' . PHP_EOL;
         break;
