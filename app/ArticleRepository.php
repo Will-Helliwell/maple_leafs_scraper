62842a19977cc31ed982539a21ff1c4122f90047
Will-Helliwell <wj.helliwell@gmail.com>
2024-09-11 12:45:58 +0800

    Only insert article if not already in the database.

--- app/ArticleRepository.php
+++ app/ArticleRepository.php
@@ -9,30 +9,40 @@ namespace App;
  * @package App
  */
 
-class ArticleRepository {
+class ArticleRepository
+{
     protected $database;
 
-    public function __construct($database_connection_details) {
+    public function __construct($database_connection_details)
+    {
         $this->database = new DatabaseConnection($database_connection_details);
     }
 
-    public function getAllArticlesGroupedBySource() {
+    public function getAllArticlesGroupedBySourceAndUrl()
+    {
         $query = "SELECT id, source_id, url FROM articles";
         $articles = $this->database->fetchAll($query);
 
         $groupedArticles = [];
         foreach ($articles as $article) {
             $source_id = $article['source_id'];
+            $url = $article['url'];
+
+            // Initialize the array for the source_id if it doesn't exist
             if (!isset($groupedArticles[$source_id])) {
                 $groupedArticles[$source_id] = [];
             }
-            $groupedArticles[$source_id][] = $article;
+
+            // Add the article to the grouped array using URL as the key
+            $groupedArticles[$source_id][$url] = $article;
         }
 
         return $groupedArticles;
     }
 
-    public function insertArticle($article) {
+
+    public function insertArticle($article)
+    {
         $query = "INSERT INTO articles (source_id, title, url, author, date_published) VALUES (?, ?, ?, ?, ?)";
         $params = [
             $article['source_id'],
@@ -43,11 +53,10 @@ class ArticleRepository {
         ];
         return $this->database->insert($query, $params);
     }
-    
 
-    public function close() {
+
+    public function close()
+    {
         $this->database->close();
     }
 }
-
-
--- scraper.php
+++ scraper.php
@@ -31,9 +31,9 @@ if ($output_type === 'database') {
     }
 }
 $sources_count = count($sources);
-echo 'sources length before = ' . $sources_count . PHP_EOL;
-echo 'sources = ' . PHP_EOL;
-pp($sources);
+// echo 'sources length before = ' . $sources_count . PHP_EOL;
+// echo 'sources = ' . PHP_EOL;
+// pp($sources);
 
 //temporarily filter array for only maple leafs
 if ($output_type === 'database') {
@@ -41,15 +41,15 @@ if ($output_type === 'database') {
         return $index === 3;
     }, ARRAY_FILTER_USE_BOTH);
     $sources_count = count($sources);
-    echo 'sources length after = ' . $sources_count . PHP_EOL;
-    echo 'sources = ' . PHP_EOL;
-    pp($sources);
+    // echo 'sources length after = ' . $sources_count . PHP_EOL;
+    // echo 'sources = ' . PHP_EOL;
+    // pp($sources);
 }
 
 // lets loop through the sources
 foreach ($sources as $source) {
-    echo 'source = ' . PHP_EOL;
-    pp($source);
+    // echo 'source = ' . PHP_EOL;
+    // pp($source);
 
     if ($output_type === 'database') {
         $source_url = $source['url'];
@@ -59,12 +59,12 @@ foreach ($sources as $source) {
     }
 
     $parsed_array = (new \App\Parser($source_url, $user_agent))->parse()->getResponse();
-    echo 'parsed_array = ' . PHP_EOL;
-    pp($parsed_array);
+    // echo 'parsed_array = ' . PHP_EOL;
+    // pp($parsed_array);
 
     $source_domain = str_replace(['https://', 'http://', 'www.'], '', $source_url);
-    echo 'source_domain = ' . PHP_EOL;
-    pp($source_domain);
+    // echo 'source_domain = ' . PHP_EOL;
+    // pp($source_domain);
 
     // merge the parsed array with the output array
     if (!empty($parsed_array)) {
@@ -77,8 +77,8 @@ foreach ($sources as $source) {
 
     // sleep between requests
     sleep(get_config('sleep_between_requests'));
-    echo PHP_EOL;
-    echo PHP_EOL;
+    // echo PHP_EOL;
+    // echo PHP_EOL;
 }
 
 // check if output array is empty
@@ -86,8 +86,8 @@ if (empty($output_array)) {
     die('No results found');
 }
 
-echo 'output_array = ' . PHP_EOL;
-pp($output_array);
+// echo 'output_array = ' . PHP_EOL;
+// pp($output_array);
 
 switch ($output_type) {
     case 'json':
@@ -99,13 +99,13 @@ switch ($output_type) {
         echo 'Output type specified as database' . PHP_EOL;
         $article_repository = new \App\ArticleRepository($database_connection_details);
         // Fetch all existing articles from the database, grouped by source_id
-        $existing_articles = $article_repository->getAllArticlesGroupedBySource();
-        // $existing_articles_length = count($existing_articles);
-        // echo 'existing_articles length = ' . $existing_articles_length . PHP_EOL;
-        // echo 'existing_articles = ' . PHP_EOL;
-        // pp($existing_articles);
+        $existing_articles = $article_repository->getAllArticlesGroupedBySourceAndUrl();
+        $existing_articles_length = count($existing_articles);
+        echo 'existing_articles length = ' . $existing_articles_length . PHP_EOL;
+        echo 'existing_articles = ' . PHP_EOL;
+        pp($existing_articles);
 
-        // Insert all scraped articles
+
         foreach ($output_array as $source_id => $articles) {
             // echo 'source_id = ' . $source_id . PHP_EOL;
             // $articles_length = count($articles);
@@ -114,10 +114,18 @@ switch ($output_type) {
             // pp($articles);
 
             foreach ($articles as $article) {
-                echo 'trying to insert into db' . PHP_EOL;
-                $article['source_id'] = $source_id;
-                $article_repository->insertArticle($article);
-                break;
+                $article_url = $article['url'];
+                echo 'source_id = ' . $source_id . PHP_EOL;
+                echo 'article_url = ' . $article_url . PHP_EOL;
+                // if article does not already exist in the database
+                if (!array_key_exists($source_id, $existing_articles) || !array_key_exists($article_url, $existing_articles[$source_id])) {
+                    echo 'article does not exist, inserting into db...' . PHP_EOL;
+                    $article['source_id'] = $source_id;
+                    $article_repository->insertArticle($article);
+                    break;
+                } else {
+                    echo 'article already exists in db' . PHP_EOL;
+                }
             }
         }
         $article_repository->close();
